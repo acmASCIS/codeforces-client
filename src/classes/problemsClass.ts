8e13ff463fe7b26f3a12ce29982f0c531e55cb27
Mina Girgis <89814001+Mina-Girgis@users.noreply.github.com>
2023-02-15 10:35:37 +0200

    Delete src/classes directory

--- src/classes/problemsClass.ts
+++
@@ -1,9 +0,0 @@
-export class Problems{
-name:Array<string>
-letter:Array<string>
-
-    constructor(name:Array<string>,letter:Array<string>){
-        this.name = name;
-        this.letter = letter;
-    }
-}
