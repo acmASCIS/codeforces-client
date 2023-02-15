b3a8538e04d3753d7464925c0b392990c20fdc80
Mina Girgis <89814001+Mina-Girgis@users.noreply.github.com>
2023-02-15 10:36:04 +0200

    Delete src/modules/contest directory

--- src/modules/contest/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.contest = void 0;
-var status_1 = require("./status/status");
-var contest = function (client) { return ({
-    status: (0, status_1.status)(client)
-}); };
-exports.contest = contest;
--- src/modules/contest/index.ts
+++
@@ -1,6 +0,0 @@
-import { status } from './status/status';
-import { ICodeforcesClient } from '../../interfaces/codeforces-client.interface';
-
-export const contest = (client: ICodeforcesClient) => ({
-  status: status(client),
-});
