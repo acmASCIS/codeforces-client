9ebce06ebc7e3095d28c01669328d21213344a88
Mina Girgis <89814001+Mina-Girgis@users.noreply.github.com>
2023-02-15 10:31:52 +0200

    Delete api-response.interface.js

--- src/interfaces/api-response.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ResponseStatus;
-(function (ResponseStatus) {
-    ResponseStatus["OK"] = "OK";
-    ResponseStatus["FAILED"] = "FAILED";
-})(ResponseStatus || (ResponseStatus = {}));
